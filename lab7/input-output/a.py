5bc929ec8ea103b61540067bec3be7405a4a4fdd
linlifxee <88711112+zhamalieva@users.noreply.github.com>
2023-03-26 18:12:09 +0600

    Add files via upload

---
+++ lab7/input-output/a.py
@@ -0,0 +1,5 @@
+import math
+a=int(input())
+b=int(input())
+c=math.sqrt((a*a)+(b*b))
+print(c)
---
+++ lab7/input-output/b.py
@@ -0,0 +1,4 @@
+a = int(input())
+print("The next number for the number {} is {}.".format(a,a+1))
+print("The previous number for the number {} is {}.".format(a,a-1))
+   
